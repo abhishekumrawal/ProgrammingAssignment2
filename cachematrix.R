e0079daf508ef0002cbb16c46fb6432e2a1360d5
abhishekumrawal <abhishekumrawal@gmail.com>
2014-06-18 20:19:21 +0530

    First update.
    The cachematrix.R file is updated.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## This is an R Code for Caching the Inverse of a Matrix.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
